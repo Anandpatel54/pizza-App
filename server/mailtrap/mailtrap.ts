b694a36c94492b9436cd428ef4c5713bd0101b8e
Anand Patel <panand054@gmail.com>
2025-04-07 08:58:48 +0530

    All part done

--- server/mailtrap/mailtrap.ts
+++ server/mailtrap/mailtrap.ts
@@ -4,8 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
  
 export const client = new MailtrapClient({token: process.env.MAILTRAP_API_TOKEN! });
-
 export const sender = {
-  email: "hello@demomailtrap.co",
+  email: "hello@demomailtrap.com",
   name: "Mailtrap Test",
 };
