07c9313631e937fbc0ebd83e012f2174a4cd05a8
Anand Patel <panand054@gmail.com>
2025-04-07 09:06:03 +0530

    All part done

--- client/src/store/useMenuStore.ts
+++ client/src/store/useMenuStore.ts
@@ -4,7 +4,7 @@ import { create } from "zustand";
 import { createJSONStorage, persist } from "zustand/middleware";
 import { useRestaurantStore } from "./useRestaurantStore";
 
-const API_END_POINT = "http://localhost:8000/api/v1/menu";
+const API_END_POINT = "https://pizza-app-1-vwmd.onrender.com/api/v1/menu";
 axios.defaults.withCredentials = true;
 
 type MenuState = {
--- client/src/store/useRestaurantStore.ts
+++ client/src/store/useRestaurantStore.ts
@@ -5,7 +5,7 @@ import { toast } from "sonner";
 import { create } from "zustand";
 import { createJSONStorage, persist } from "zustand/middleware";
 
-const API_END_POINT = "http://localhost:8000/api/v1/restaurant";
+const API_END_POINT = "https://pizza-app-1-vwmd.onrender.com/api/v1/restaurant";
 axios.defaults.withCredentials = true;
 
 
